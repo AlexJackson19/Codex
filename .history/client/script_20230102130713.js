bfdff7f4cfde24c3ad53991558c327b3597670c6
AlexJAckson19 <wlamar715@gmail.com>
2023-01-02 13:17:05 -0500

    update the server url

--- .history/client/script_20230102130713.js
+++ .history/client/script_20230102130713.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://codex-eqms.onrender.com/', {
+    const response = await fetch('https://codex-eqms.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
