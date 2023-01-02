d7de714c43049ddb90111fbe17fcf53cede25614
876Gad <76132707+AlexJackson19@users.noreply.github.com>
2023-01-02 14:41:15 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import * as dotenv from "dotenv";
-import cors from "no-cors";
+import cors from "cors";
 import { Configuration, OpenAIApi } from "openai";
 
 dotenv.config();
